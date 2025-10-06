195602b831e9a112bc6886a901718e9af0ba9a85
BaseZ-code <zigma.pn@gmail.com>
2025-10-06 17:42:23 +0700

    Implement Spotify downloader functionality with concurrent downloads and metadata handling

--- downloader.py
+++ downloader.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 from dotenv import load_dotenv
 from mutagen.id3 import ID3, TPE1 , TIT2, TALB, TYER
 import spotipy
+import concurrent.futures
 from spotipy.oauth2 import SpotifyClientCredentials
 load_dotenv()
 
@@ -17,7 +18,11 @@ Path.cwd() / "Spotify-Dowloader"
 auth_manager = SpotifyClientCredentials( 
     client_id=os.getenv('CLIENT_ID'),    client_secret=os.getenv('CLIENT_SECRET')
     )
-sp = spotipy.Spotify(auth_manager=auth_manager)
+
+try:
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+except spotipy.SpotifyException as e:
+    print("Spotify API authentication failed. Please check your CLIENT_ID and CLIENT_SECRET.")  
 
 @dataclass
 class Track:
@@ -28,8 +33,8 @@ class Track:
 
 # Downloader
 class SpotifyDownloader():
-    global sp
-    def __init__(self, url:str) -> None:
+    def __init__(self,sp:vars, url:str) -> None:
+        self.sp = sp
         self.url = url
         self.output_path = Path.home() / "Music" / "Songs"
         self.cookies_path = Path.home() / "OneDrive" / "Documents" / "cookies.txt"
@@ -57,25 +62,29 @@ class SpotifyDownloader():
         metadata = []
         # Check if the url is a playlist or a track
         limit_tracks = 100
+        current_track = 0
         if playlist:
-            total_tracks = sp.playlist_tracks(self.url, limit=1)['total']
-            for offset in range(0, total_tracks, 1):
-                tracks = sp.playlist_tracks(self.url, additional_types=('track',), offset=offset, limit=limit_tracks)
-                print(tracks)
-
+            total_tracks = self.sp.playlist_tracks(self.url, limit=1)['total']
+            for offset in range(0, total_tracks, limit_tracks):
+                tracks = self.sp.playlist_tracks(self.url, additional_types=('track',), offset=offset, limit=limit_tracks)
                 for item in tracks["items"]:
-                    track = item['track']
+                    current_track+=1
+                    track = item["track"]
                     if track:
                         metadata.append(Track(
                                     track['name'], 
                                     track['artists'][0]['name'], 
                                     track["album"]["name"],
                                     track["album"]["release_date"]))
+                    else:
+                        print("Skipping a local track or unavailable track.")
+                        continue
+                    print(f"Fetching data.. {current_track} / {total_tracks}")
 
-                print(f"Fetching data.. {offset+1} / {total_tracks}")
+                
 
         elif not playlist:
-            tracks = sp.track(self.url)
+            tracks = self.sp.track(self.url)
             metadata.append(Track(tracks["name"], 
                         tracks["artists"][0]["name"], 
                         tracks["album"]["name"],
@@ -84,35 +93,11 @@ class SpotifyDownloader():
             raise KeyError("Unexpected Spotify Url")
         return metadata
 
-    # Use for changing file name
-    def change_filename(self, title:str) -> None:
-        counter = 0
-        sanitized_title = self.sanitize_filename(title)
-        files = list(self.output_path.iterdir())
-        ext = "mp3"
-        recently_added_file = max(files, key=lambda f: (self.output_path / f).stat().st_mtime)
-        
-        old_name = self.output_path / recently_added_file
-        new_name = self.output_path / f"{sanitized_title}.{ext}"
-
-        while new_name.exists():
-            counter += 1
-            new_countered_name = self.output_path / f"{sanitized_title}({counter}).{ext}"
-            if new_countered_name.exists():
-                continue
-            else:
-                new_name.rename(new_countered_name)
-                print(f"Changing name from '{new_name}' to '{new_countered_name}'")
-                return
-
-        print(f"Changing name from '{old_name}' to '{new_name}'")
-        old_name.rename(new_name)
-
     def DownloaderOptions(self):
         ydl_opts = {
             'default_search': 'ytsearch',
             'noplaylist' : True,
-            'noprogress' : False,
+            'noprogress' : True,
             'no_warnings' : True,
             "cookiefile": f"{self.cookies_path}",
             'postprocessors': [{
@@ -129,13 +114,18 @@ class SpotifyDownloader():
     # Yt_dlp Download function
     def dowload(self, data:Track) -> None:
         # Download options
+        sanitized_title = self.sanitize_filename(data.title)
+        sanitized_artist = self.sanitize_filename(data.artist)
+        output_template = self.output_path / f"{sanitized_title}.%(ext)s"
+
         ydl_opts = self.DownloaderOptions()
+        ydl_opts['outtmpl'] = str(output_template)
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.add_post_processor(Metadata_replacerPP(data), when='post_process')
             ydl.download([f"{data.title} {data.artist} lyrics"])
 
         time.sleep(0.5)
-        self.change_filename(data.title)
 
 # Class which will be used in yt_dlp Postprocessor -> Use to add metadata to a file.
 class Metadata_replacerPP(yt_dlp.postprocessor.PostProcessor):
@@ -162,16 +152,24 @@ def clear_terminal():
 def main():
     clear_terminal()
     url = input("Enter Spotify URL(Song/Playlist): ").strip()
-    track = SpotifyDownloader(url)
-    track.output_path.mkdir(parents=True, exist_ok=True)  # Create the output directory if it doesn't exist
-    metadata = track.track_metadata(playlist=track.spotify_url_parser(url))
+    track_downloader = SpotifyDownloader(sp, url)
+    track_downloader.output_path.mkdir(parents=True, exist_ok=True)  # Create the output directory if it doesn't exist
+    metadata = track_downloader.track_metadata(playlist=track_downloader.spotify_url_parser(url))
+    
     if not metadata:
         print("No metadata found for the provided URL.")
         sys.exit(1)
-    for data in metadata:
-        track.dowload(data)
-        print(f"Downloaded: {data.title} - {data.artist}")
-        print("\n")
+    else:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            future_to_track = {executor.submit(track_downloader.dowload, data): data for data in metadata}
+            for future in concurrent.futures.as_completed(future_to_track):
+                data = future_to_track[future]
+                try:
+                    future.result()
+                    print(f"Downloaded: {data.title} by {data.artist}")
+                except Exception as e:
+                    print(f"Error downloading {data.title} by {data.artist}: {e}")
+        print("All downloads completed.")
 
 try:
     if __name__ == "__main__":
