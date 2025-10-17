3d86579a6201ae496e7872e1a26ca3f81fe74797
BaseZ-code <zigma.pn@gmail.com>
2025-10-17 15:47:58 +0700

    Add initial app structure and UI components for Spotify Downloader

--- src/app.py
+++ src/app.py
@@ -1,2 +1,28 @@
 from downloader import SpotifyDownloader
+from PIL import Image, ImageTk 
+import customtkinter
 import os
+import sys
+import time
+
+class myApp(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Spotify Downloader")
+        self.geometry("500x300")
+        self.grid_columnconfigure((0, 1, 2), weight=1)
+        self.resizable(False, False)
+
+        self.button = customtkinter.CTkButton(self, text="Download", command=self.button_callback, fg_color=("#DB3E39", "#821D1A"))
+        self.button.grid(row=0, column=1, padx=50, pady=20, sticky="ew")
+
+        self.button = customtkinter.CTkButton(self, text="Download", command=self.button_callback, fg_color=("#DB3E39", "#821D1A"))
+        self.button.grid(row=0, column=1, padx=50, pady=20, sticky="ew")
+
+    def button_callback(self):
+            print("Something")
+
+        
+
+app = myApp()
+app.mainloop()
